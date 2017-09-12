3bb5207613a2cfc7f44182cdf2a88e29d1ac574e
pdunbar1999 <peterdunbar1999@gmail.com>
2017-09-11 20:58:52 -0500

    Almost working, letters are appearing twice, but all logic works

--- pygame_hangman.py
+++ pygame_hangman.py
@@ -29,41 +29,119 @@ basePole = pygame.transform.scale(basePole, (800,500))
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 #Now, gameDisplay is the screen we will be working with
 
+global lives_remaining
+lives_remaining = 0 #number of lives
 
 def add_body_parts():
-    if count == 1: #head
+    global lives_remaining #global so it knows its a global
+    lives_remaining += 1 #adds 1 whenever the function is called
+    if lives_remaining == 1: #head
         pygame.draw.circle(gameDisplay, black, (634, 106), 50)
         pygame.display.update()
-    if count == 2: #body
+    if lives_remaining == 2: #body
         pygame.draw.line(gameDisplay, black, (634,156), (634,256), 5)
         pygame.display.update()
-    if count == 3:#left leg
+    if lives_remaining == 3:#left leg
         pygame.draw.line(gameDisplay, black, (634,256), (552, 352), 5)
         pygame.display.update()
         #start where body finished
-    if count == 4:#right leg
+    if lives_remaining == 4:#right leg
         pygame.draw.line(gameDisplay, black, (634,256), (716,352), 5)
         pygame.display.update()
         #start where body finished
-    if count == 5:#left arm
+    if lives_remaining == 5:#left arm
         pygame.draw.line(gameDisplay, black, (634,206), (584,206), 5)
         pygame.display.update()
         #start at middle of body
-    if count == 6:#right arm
+    if lives_remaining == 6:#right arm
         pygame.draw.line(gameDisplay, black, (634,206), (684, 206), 5)
         pygame.display.update()
-    
 
+
+def ask_for_letter():
+    #checks what key was pressed down
+    if event.key == pygame.K_a: #checks what key (K_s), s is key
+        letter_check('a') #passes the pressed key into letter_check
+    elif event.key == pygame.K_b:
+        letter_check('b')
+    elif event.key == pygame.K_c:
+        letter_check('c')
+    elif event.key == pygame.K_d:
+        letter_check('d')
+    elif event.key == pygame.K_e:
+        letter_check('e')
+    elif event.key == pygame.K_f:
+        letter_check('f')
+    elif event.key == pygame.K_g:
+        letter_check('g')
+    elif event.key == pygame.K_h:
+        letter_check('h')
+    elif event.key == pygame.K_i:
+        letter_check('i')
+    elif event.key == pygame.K_j:
+        letter_check('j')
+    elif event.key == pygame.K_k:
+        letter_check('k')
+    elif event.key == pygame.K_l:
+        letter_check('l')
+    elif event.key == pygame.K_m:
+        letter_check('m')
+    elif event.key == pygame.K_n:
+        letter_check('n')
+    elif event.key == pygame.K_o:
+        letter_check('o')
+    elif event.key == pygame.K_p:
+        letter_check('p')
+    elif event.key == pygame.K_q:
+        letter_check('q')
+    elif event.key == pygame.K_r:
+        letter_check('r')
+    elif event.key == pygame.K_s:
+        letter_check('s')
+    elif event.key == pygame.K_t:
+        letter_check('t')
+    elif event.key == pygame.K_u:
+        letter_check('u')
+    elif event.key == pygame.K_v:
+        letter_check('v')
+    elif event.key == pygame.K_w:
+        letter_check('w')
+    elif event.key == pygame.K_x:
+        letter_check('x')
+    elif event.key == pygame.K_y:
+        letter_check('y')
+    elif event.key == pygame.K_z:
+        letter_check('z')       
+    
+def letter_check(letter):
+    guess = letter #whatever the key specified is
+    check = 0 #checks if at least the letter is in the word once
+    for x in range(0,len(word)):
+        if word[x] == guess: 
+            del string_word[x]
+            string_word.insert(x, guess)
+            check += 1
+    if check == 0: #if its 0, means the letter wasn't right
+        add_body_parts() #adds a body part
+
+
+def display_word(): #displays the incompleted word at the bottom
+    q = pygame.font.SysFont('none', 100) #specifes the font wanted and the font size
+    x = q.render('  '.join(string_word), True, white) #.join turns the string into a list with spaces inbetween it.
+    gameDisplay.blit(x, (100,500)) #displays the word onto the screen
+    
 
 gameDisplay.blit(basePole, (0,0))
 #displays background image once
 
-
-####this is a test
-
-
-count = 0
 gameExit = False
+
+word = 'mother' #TESTING PURPOSES ONLY
+string_word = []#reserving an empty list to put the word in
+
+for x in range(0,len(word)):
+    string_word.append("_")
+    #adds _ to however many letters are needed
 
 while not gameExit: #while True
     for event in pygame.event.get():
@@ -75,13 +153,17 @@ while not gameExit: #while True
             quit()
 
         if event.type == pygame.KEYDOWN:#checks if key is pressed down
-            if event.key == pygame.K_p: #checks what key (K_s), s is key
-                
-                count = count + 1
-                add_body_parts()
-                    
-                
+            ask_for_letter()
 
+
+    if lives_remaining == 6:
+        print("You lost")
+        gameExit = True
+        pygame.QUIT
+        quit()
+            
+               
+    display_word()
     pygame.display.update() #updates the screen so we can see it
 
     
